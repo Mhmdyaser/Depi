7b77f20deaab5729d32da918ad26245cffe7c459
Eng Mohamed Yaser <mhmdyaser97.is@gmail.com>
2025-08-29 23:18:24 +0300

    Add files via upload

---
+++ MNF3_SWD5_S2/OOP/calc extension method/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace calc_extension_method
+{
+    using System;
+
+  
+    public delegate double CalcOperation(double x, double y);
+
+    public static class Extensions_Methood
+    {
+        public static double Calculate(this double x, double y, CalcOperation op)
+        {
+            return op(x, y);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+       
+            CalcOperation add = (a, b) => a + b;
+            CalcOperation subtract = (a, b) => a - b;
+            CalcOperation multiply = (a, b) => a * b;
+            CalcOperation divide = (a, b) =>  a / b ;
+
+            double num1 = 10;
+            double num2 = 25;
+
+            
+            Console.WriteLine(num1.Calculate(num2, add));       
+            Console.WriteLine(num1.Calculate(num2, subtract));  
+            Console.WriteLine(num1.Calculate(num2, multiply));  
+            Console.WriteLine(num1.Calculate(num2, divide));    
+        }
+    }
+}
